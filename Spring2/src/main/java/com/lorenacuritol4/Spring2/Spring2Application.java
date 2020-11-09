0ce91988ce4f8791ee5a84a0ca8e85079de35db1
lmcu <infomaticloregmail.com>
2020-11-09 15:25:29 -0300

    completando tareas 1. Close #1

---
+++ Spring2/src/main/java/com/lorenacuritol4/Spring2/Spring2Application.java
@@ -0,0 +1,13 @@
+package com.lorenacuritol4.Spring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring2Application.class, args);
+	}
+
+}
---
+++ Spring2/src/test/java/com/lorenacuritol4/Spring2/Spring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorenacuritol4.Spring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
